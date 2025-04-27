65762d639fe144d3a7082d241cebe58b2d7e6987
ROYWOOD <ronald.brooks206@gmail.com>
2025-04-27 23:32:01 +0300

    поправка условия и фикс бага с лоадером

--- vue/src/views/FavouritesView.vue
+++ vue/src/views/FavouritesView.vue
@@ -5,7 +5,7 @@
 
 
         <section 
-        v-if="games.length != 0"
+        v-if="games.length > 0"
         class="text-white">
             
             <main class="flex flex-wrap justify-around ">
@@ -13,6 +13,7 @@
                     v-for="game in games" 
                     :key="game.id_game" 
                     :game="game"
+                    :fetchFavorite="fetchFavorite"
                     isFavor />
             </main>
              
@@ -62,22 +63,32 @@ export default {
     },
     methods: {
         async fetchFavorite() {
-            const respounse = await fetch('/gamestore/favourites/games',{
-                headers: {
-                    'Authorization': `Bearer ${this.gameStore.token}`,
+
+            try {
+                this.isLoader = false; // стартуем лоадер
+                const respounse = await fetch('/gamestore/favourites/games',{
+                    headers: {
+                        'Authorization': `Bearer ${this.gameStore.token}`,
+                    }
+                });
+
+                const data = await respounse.json();
+
+                if(respounse.ok) {
+                    this.games = data;
+                    this.isLoader = true;
+                    return;
                 }
-            });
+                else {
+                    this.gameStore.showError(data.error);
+                }
+            }   catch(error) {
+                this.isLoader = true;
+                console.error('Ошибка при получении избранного:', error);
 
-            const data = await respounse.json();
-
-            if(respounse.ok) {
-                this.games = data;
-                this.isLoader = !this.isLoader;
-                return;
             }
-            else {
-                this.gameStore.showError(data.error);
-            }
+            
+            
         },
     },
     async created() {
