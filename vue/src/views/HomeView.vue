677353e1d22d2acdb4f41416c7ec8012d5e18d13
ROYWOODEN <ronald.brooks2006@gmail.com>
2025-05-03 14:03:39 +0300

    hotfix media

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -9,7 +9,7 @@
         </div>
         
         <main v-else-if="gameStore.games.length > 0" class="">
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-5 xl:gap-1 !px-8 !py-3">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-5 xl:gap-1 !px-8 !py-3">
                     <GamesItem
                     v-for="game in paginationGames"
                     :key="game.id_game"
